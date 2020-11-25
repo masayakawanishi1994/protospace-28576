1358f30246d5abc5e1e9631678247bbe219fcab9
masayakawanishi1994 <seremade1.2.3@gmail.com>
2020-11-25 14:09:03 +0900

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,4 +14,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
-end
+# a
